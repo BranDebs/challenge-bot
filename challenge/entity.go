36ca38f93410b1d56c237edb7551e98c942f86db
brandonyeoxg <brandonyeoxg@gmail.com>
2021-10-09 23:38:41 +0800

    feat(entity): add challenge entities

---
+++ challenge/entity.go
@@ -0,0 +1,30 @@
+package challenge
+
+type Challenge struct {
+	ID        uint64
+	Name      string
+	UserIDs   []uint64
+	StartDate uint64
+	EndDate   uint64
+	Schema    []byte
+}
+
+type User struct {
+	ID       uint64
+	Username string
+}
+
+type Goal struct {
+	ID          uint64
+	UserID      uint64
+	ChallengeID uint64
+	Value       []byte
+}
+
+type Progress struct {
+	ID          uint64
+	UserID      uint64
+	ChallengeID uint64
+	Value       []byte
+	Date        uint64
+}
