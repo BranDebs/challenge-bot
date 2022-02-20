9f884a762277e0e51d3e787878c1bdfd89582c54
brandonyeoxg <brandonyeoxg@gmail.com>
2022-02-20 14:30:18 +0800

    refactor(progress): use pointer for optimisation

--- internal/domain/progress/progress.go
+++ internal/domain/progress/progress.go
@@ -2,7 +2,7 @@ package progress
 
 import "github.com/BranDebs/challenge-bot/internal/domain/condition"
 
-var EmptyProgress = Progress{}
+var EmptyProgress = &Progress{}
 
 type Progress struct {
 	ID          uint64
@@ -13,7 +13,7 @@ type Progress struct {
 	UpdatedAt   uint64
 }
 
-func New(id uint64, userID uint64, challengeID uint64, data []byte, isGoal bool, updatedAt uint64) (Progress, error) {
+func New(id uint64, userID uint64, challengeID uint64, data []byte, isGoal bool, updatedAt uint64) (*Progress, error) {
 	if id == 0 || userID == 0 || challengeID == 0 {
 		return EmptyProgress, ErrInvalidProgress
 	}
@@ -21,7 +21,7 @@ func New(id uint64, userID uint64, challengeID uint64, data []byte, isGoal bool,
 	// make data into conditions
 	values := make([]condition.Condition, 0)
 
-	return Progress{
+	return &Progress{
 		ID:          id,
 		UserID:      userID,
 		ChallengeID: challengeID,
